9573b923587d11a03115c02a37518b076535ef44
RetroWranger <csaw700@icloud.com>
2025-08-24 01:40:58 -0400

    Initial Commit

---
+++ BDAA Design Suite/BDAA_Design_SuiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDAA_Design_SuiteApp.swift
+//  BDAA Design Suite
+//
+//  Created by Cory on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BDAA_Design_SuiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BDAA Design Suite/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  BDAA Design Suite
+//
+//  Created by Cory on 8/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
